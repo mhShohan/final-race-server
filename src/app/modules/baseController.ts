87a1fd5da9fe4aefbd6e0eae6d8efe7e5f2018c2
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-01-24 00:19:16 +0600

    trying to create baseController

--- src/app/modules/baseController.ts
+++ src/app/modules/baseController.ts
@@ -1,12 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-class BaseController {
-  protected controller: any;
-  protected services: any;
+import asyncHandler from "../lib/asyncHandler";
+import STATUS from "../lib/httpStatus";
+import sendResponse from "../lib/sendResponse";
+import BaseServices from "./baseServices";
 
-  constructor(controller: any, services: any) {
-    this.controller = controller;
+class BaseController<T> {
+  constructor(protected services: BaseServices<T>, protected name: string) {
     this.services = services;
+    this.name = name
   }
+
+  public create = asyncHandler(async (req, res) => {
+    const result = await this.services.create(req.body);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: STATUS.CREATED,
+      message: `${this.name} Created Successfully`,
+      data: result,
+    });
+  })
 }
 
 export default BaseController;
