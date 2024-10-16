b1534f4ddfdf71d77e6fdded86f0280ca615a83d
mhShohan <mehdihasanshohan25@gmail.com>
2024-10-16 09:49:24 +0600

    feat/library - library services implemented

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -9,7 +9,9 @@ bookRoutes.use(verifyAuth)
 
 bookRoutes.post('/', verifyRole([ 'DEPARTMENT_OPERATOR' ]), bookController.create);
 bookRoutes.get('/', verifyRole([ 'DEPARTMENT_OPERATOR' ]), bookController.getAll);
+bookRoutes.get('/student', verifyRole([ 'STUDENT' ]), bookController.getBookOfStudent);
 bookRoutes.get('/:id', verifyRole([ 'DEPARTMENT_OPERATOR' ]), bookController.getSingle);
-
+bookRoutes.patch('/:id/provide', verifyRole([ 'DEPARTMENT_OPERATOR' ]), bookController.provideBook);
+bookRoutes.patch('/:id/borrowedBack', verifyRole([ 'DEPARTMENT_OPERATOR' ]), bookController.borrowedBack);
 
 export default bookRoutes;
